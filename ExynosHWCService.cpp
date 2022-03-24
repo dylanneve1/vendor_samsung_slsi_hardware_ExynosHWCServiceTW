aabd946a5a378a5482238ccac55079c5b28c59ff
Dylan Neve <dylanneve1@gmail.com>
2022-03-24 01:23:16 +0000

    Create main function and log

---
+++ ExynosHWCService.cpp
@@ -0,0 +1,12 @@
+// Define log tag
+#define LOG_TAG "exynoshwc@1.0-service"
+
+// Inclusions
+#include <log/log.h>
+
+// Main
+int main()
+{
+	ALOGD("Exynos HWC Service 1.0 is starting.");
+	return 0;
+}
